425a4a6da56b5a157e3393e230da6773099a7354
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-06-02 15:18:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
+
